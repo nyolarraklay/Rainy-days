c8c3488daf6db45caa327fb4ffefdf4b814a1d10
nyolarraklay <nyolosorii@gmail.com>
2023-03-16 13:31:20 +0100

    ok fine edited again

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+const contact = document.querySelector(".SM-box1 p");
+const cartProduct = document.querySelector(".SM-box1 p");
+
+function redirectPageIndex() {
+  location.href = "cart.html";
+}
+
+cartProduct.addEventListener("click", redirectPageIndex);
+
+function redirectPageIndex() {
+  location.href = "contact.html";
+}
+
+contact.addEventListener("click", redirectPageIndex);
