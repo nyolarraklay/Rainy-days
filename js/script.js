60403dd1575f62a548c5cf1cd1a1d10020c2cfaf
nyolarraklay <nyolosorii@gmail.com>
2023-03-20 12:43:46 +0100

    wcag

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const mainElement = document.querySelector(".home-page");
 
-
 const div = document.createElement("div");
 div.classList.add("blur");
 div.classList.add("saleContainer");
@@ -8,18 +7,19 @@ const divText = document.createElement("div");
 divText.classList.add("divText");
 const h2 = document.createElement("h2");
 h2.innerText = "Search what suits you";
-const p = document.createElement("p");
-p.innerText = "Find branded jacket for sale or new arrivals";
 const divInput = document.createElement("div");
 divInput.classList.add("divInput");
+const label = document.createElement("label");
+label.for = "find";
+label.innerText = "Find branded jacket for sale or new arrivals";
 const input = document.createElement("input");
 input.className = "search";
-input.placeholder ="Search"
+input.placeholder = "Search";
 const button1 = document.createElement("button");
 button1.innerText = "New Arrivals";
 const button2 = document.createElement("button");
 button2.innerText = "For Sale";
-divText.append(h2, p);
+divText.append(h2, label);
 divInput.append(input, button1, button2);
 div.append(divText, divInput);
 
@@ -29,12 +29,8 @@ button1.addEventListener("click", redirectPage);
 button2.addEventListener("click", redirectPage);
 input.addEventListener("submit", redirectPage);
 
-
-function redirectPage(){
-  location.href=("products.html");
+function redirectPage() {
+  location.href = "products.html";
 }
 
 // validation
-
-
-
