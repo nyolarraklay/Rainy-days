75f02409d2a1fd41db2bf5c78c230d38a3a6d300
nyolarraklay <nyolosorii@gmail.com>
2023-03-20 14:12:04 +0100

    wave again

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,29 @@
-const contact = document.querySelector(".SM-box1 p");
-const cartProduct = document.querySelector(".SM-box1 p");
+const contact = document.querySelector(".SM-box1");
+const cartProduct = document.querySelector(".SM-box2 p");
 
-function redirectPageIndex() {
+const shoppingBox = document.querySelector(".SM-box3");
+const ourProduct = document.querySelector(".productBox");
+
+function redirectToCart() {
   location.href = "cart.html";
 }
 
-cartProduct.addEventListener("click", redirectPageIndex);
+cartProduct.addEventListener("click", redirectToCart);
 
-function redirectPageIndex() {
+function redirectToContact() {
   location.href = "contact.html";
 }
 
-contact.addEventListener("click", redirectPageIndex);
+contact.addEventListener("click", redirectToContact);
+
+function redirectToAbout() {
+  location.href = "contact.html";
+}
+
+shoppingBox.addEventListener("click", redirectToAbout);
+
+function redirectToProduct() {
+  location.href = "products.html";
+}
+
+ourProduct.addEventListener("click", redirectToProduct);
