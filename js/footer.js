b2066207d13b41b71382d206d25fb98defb2ac4c
nyolarraklay <nyolosorii@gmail.com>
2023-03-20 14:52:07 +0100

    missing h1

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,6 @@ const cartProduct = document.querySelector(".SM-box2 p");
 
 const shoppingBox = document.querySelector(".SM-box3");
 const ourProduct = document.querySelector(".productBox");
-const button = document.querySelector(".homeButton");
 
 function redirectToCart() {
   location.href = "cart.html";
@@ -28,4 +27,3 @@ function redirectToProduct() {
 }
 
 ourProduct.addEventListener("click", redirectToProduct);
-button.addEventListener("click", redirectToProduct);
