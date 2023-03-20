dc10dbf5f6d92b39a5ce39e99277da62b9b4a1cc
nyolarraklay <nyolosorii@gmail.com>
2023-03-20 14:20:35 +0100

    buttons

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,7 @@ const cartProduct = document.querySelector(".SM-box2 p");
 
 const shoppingBox = document.querySelector(".SM-box3");
 const ourProduct = document.querySelector(".productBox");
+const button = document.querySelector(".homeButton");
 
 function redirectToCart() {
   location.href = "cart.html";
@@ -27,3 +28,4 @@ function redirectToProduct() {
 }
 
 ourProduct.addEventListener("click", redirectToProduct);
+button.addEventListener("click", redirectToProduct);
